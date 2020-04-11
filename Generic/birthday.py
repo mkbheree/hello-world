a4c1889bf3857191e66052888ae5cbb3c05271ad
Meher Kiran <meher.bheree@gmail.com>
2020-04-11 19:41:57 +0530

    Dictionary

---
+++ Generic/birthday.py
@@ -0,0 +1,26 @@
+# dictionary
+
+birthdays = {'ajay raj': 'June 1', 'jaya prakash': 'Jan 22', 'prasanth kumar': 'Apr 04'}
+print(list(birthdays))  # print the keys of dict
+'''dict.keys() & dict.values() returns a tuple(immutable)'''
+while True:
+    name = input('Enter your friend name for birthday details (blank to quit)')
+    if name =='':
+        break
+    if name in birthdays.keys():
+        print(birthdays[name.lower()] + ' is the birthday of ' + name)
+    else:
+        print('Name is\'nt found in the database,please enter the birth date')
+        birthdays[name.lower()] = input()
+        print('updated database')
+        for key, value in birthdays.items():
+            print(key +' : '+value)
+
+'''
+get(key,val) - returns the val in case of absence of key, instead of key error
+setdefault(key,val) - sets a default value to a key, if key is'bt present in the dict
+pprint.pprint()- helps to print dictionary in a pretty manner
+'''
+
+
+
